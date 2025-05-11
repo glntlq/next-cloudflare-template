ced0abe85372f6f241a6a4b63ce48daab65481bb
Felix <2072738187yu@gamil.com>
2025-05-11 15:26:13 +0800

    feat: server less test

---
+++ app/[locale]/server-less-test.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from 'react'
+
+import { ServerLessActiveTimesTest } from '@/actions/test'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export const SeverLessTestComponent = () => {
+  const [value, setValue] = useState('')
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value
+    // Only update the value if it's an integer or empty
+    if (/^\d*$/.test(newValue)) {
+      setValue(newValue)
+    }
+  }
+
+  return (
+    <>
+      <Input value={value} onChange={handleInputChange} />
+      <Button
+        onClick={() => {
+          ServerLessActiveTimesTest(Number(value))
+        }}
+      >
+        {'test'}
+      </Button>
+    </>
+  )
+}
