4d9452507d8b0f1d3ce06b28efffea1a7e990c22
Felix <2072738187yu@gamil.com>
2025-05-17 01:41:29 +0800

    feat: remove durable

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,18 @@ import createNextIntlPlugin from 'next-intl/plugin'
 initOpenNextCloudflareForDev()
 
 const nextConfig: NextConfig = {
-  devIndicators: false
+  devIndicators: false,
+  useCache: true,
+  reactCompiler: true,
+  ppr: true,
+  typedRoutes: false,
+  dynamicIO: false,
+  experimental: {
+    staleTimes: {
+      dynamic: 3600,
+      static: 3600
+    }
+  }
 }
 
 const withNextIntl = createNextIntlPlugin({
